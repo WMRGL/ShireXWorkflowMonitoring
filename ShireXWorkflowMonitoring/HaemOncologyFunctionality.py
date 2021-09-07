db84856f6c4d9a836d29abf900f0971cfdac5904
pmasonBWC <paul.mason22@nhs.net>
2021-09-07 17:29:01 +0100

    Mods following Joanne's feedback, part 1.

--- ShireXWorkflowMonitoring/HaemOncologyFunctionality.py
+++ ShireXWorkflowMonitoring/HaemOncologyFunctionality.py
@@ -56,20 +56,30 @@ class BMTSearch(TemplateView):
             for _row in _workflowCases:
                 _labNumber = _row['LABNO']
                 _row['WORKSHEETS'] = ""
+                _row['RESULTS_OUTSTANDING'] = "no"
+                _row['WORKSHEET_OUTSTANDING'] = "yes"
 
                 if _labNumber != _previousLabNumber:
+                    #If the lab number is different, compile the worksheet/test/result information
                     _wsResults = self.dataServices.GetSampleWorksheetResults(_labNumber)
 
                     _worksheetList = ["",]
                     _testResultList = ["",]
 
                     for _wsRow in _wsResults:
+                        #For each worksheet/test/result
                         _worksheet = _wsRow['WORKSHEET']
                         _test = _wsRow['TEST']
                         _result = _wsRow['RESULT']
 
+                        _row['WORKSHEET_OUTSTANDING'] = "no"
+
+                        if (_result == None) or (_result == ''):
+                            _row['RESULTS_OUTSTANDING'] = "yes"
+                            _result = "No result"
+
                         #If the worksheet is not in the list
-                        #i.e. index() fails, add it
+                        #i.e. index() fails, add it, otherwise move on
                         try:
                             _worksheetList.index(_worksheet)
                         except:
