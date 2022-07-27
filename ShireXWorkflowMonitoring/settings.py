b34ff30a7a04e1b294dc3ee873142d4d2dc4762e
LoxtonM <martin.loxton@nhs.net>
2022-07-27 14:40:43 +0100

    Version 3.2 fix to WGS workflow part 3 (null trapping for worksheet functionality and logout timer fix)

--- ShireXWorkflowMonitoring/settings.py
+++ ShireXWorkflowMonitoring/settings.py
@@ -38,8 +38,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['10.147.33.230']
 
 # Application definition
 
