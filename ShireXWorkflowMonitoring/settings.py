7ea4f8268e627d657c4a92268713f89d8ad5bf77
ReubenMonaghan <reuben.monaghan@nhs.net>
2024-10-11 16:51:59 +0100

    dyslexia.js removed

--- ShireXWorkflowMonitoring/settings.py
+++ ShireXWorkflowMonitoring/settings.py
@@ -158,6 +158,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [BASE_DIR / 'static',]
+STATICFILES_DIRS = [BASE_DIR / 'ShireXWorkflowMonitoring/static',]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
